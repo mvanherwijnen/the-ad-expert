0492f551e80c38daa111b9a5685d4cb2615a8747
Duin002 <max.vanduin@wonderkind.com>
2019-06-06 18:24:16 +0200

    Ad initial push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="ad-container">
+    <facebook-ad>
+      <template #ad-title>
+        <h4 v-html="highLightedTitle"></h4>
+      </template>
+      <template #ad-message>
+        <p class="message" v-html="highLightedMessage"></p>
+      </template>
+      <template #vacancy-title >
+        <h3 v-html="highLightedVacancyTitle"></h3>
+      </template>
+      <template #vacancy-description>
+        <p v-html="highLightedVacancyDescription"/>
+      </template>
+    </facebook-ad>
+    <input type="text" v-model="query">
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import FacebookAd from './FacebookAd';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+  export default {
+    components: {
+      FacebookAd,
+    },
+    data() {
+      return {
+        query: 'Lorem',
+        ad: {
+          title: 'Lorem ipsum dolor sit amet',
+          message: 'Amet aspernatur culpa, cumque debitis dolor eos harum nemo nihil odio quia quidem',
+          vacancyTitle: 'Lorem ipsum dolor sit amet',
+          vacancyDescription: 'Amet aspernatur culpa, cumque debitis dolor eos harum nemo nihil odio quia quidem',
+        }
+      }
+    },
+    computed: {
+      highLightedTitle() {
+        return this.highlight('title');
+      },
+      highLightedMessage() {
+        return this.highlight('message');
+      },
+      highLightedVacancyTitle() {
+        return this.highlight('vacancyTitle');
+      },
+      highLightedVacancyDescription() {
+        return this.highlight('vacancyDescription');
+      },
+    },
+    methods: {
+      highlight(field) {
+        console.log('trigger')
+        if(!this.query) {
+          return this.ad[field];
+        }
+        return this.ad[field].replace(new RegExp(this.query, "gi"), match => {
+          return '<span class="highlightText">' + match + '</span>';
+        });
+      }
+    },
+  }
+</script>
+<style>
+.ad-container {
+  height: 100%;
+  width: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fafafa;
+  justify-content: center;
+  padding: 2rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.highlightText {
+  background-color: yellow;
+  display: inline;
 }
 </style>
