4e3324595adea08f0048471455a5010515fb741e
Duin002 <max.vanduin@wonderkind.com>
2019-06-07 15:25:45 +0200

    Restore filled in inputs

--- src/store.js
+++ src/store.js
@@ -5,18 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    ad: {
-      title: 'Apple',
-      message: 'Start with the benefits of the advertised job',
-      vacancyTitle: 'Put a descriptive job title here',
-      vacancyDescription: 'You can enter your call to action here',
-    },
     // ad: {
     //   title: 'Apple',
-    //   message: 'Apple is looking for talent. Do you want to earn a competitive salary ? Are you ready to say goodbye to your nine to five mentality ? Apply now and join our family !',
-    //   vacancyTitle: 'Are you a retail jedi ?',
-    //   vacancyDescription: 'Apple is looking for talent. Join us now!',
+    //   message: 'Start with the benefits of the advertised job',
+    //   vacancyTitle: 'Put a descriptive job title here',
+    //   vacancyDescription: 'You can enter your call to action here',
     // },
+    ad: {
+      title: 'Apple',
+      message: 'Apple is looking for talent. Do you want to earn a competitive salary ? Are you ready to say goodbye to your nine to five mentality ? Apply now and join our family !',
+      vacancyTitle: 'Are you a retail jedi ?',
+      vacancyDescription: 'Apple is looking for talent. Join us now!',
+    },
   },
   mutations: {
     lelijkeHack(state) {
